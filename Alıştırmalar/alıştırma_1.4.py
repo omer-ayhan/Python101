1927d30181037f0a10cac55174bce0c325c875f6
Ömer Ayhan <om.ayhan247@gmail.com>
2021-07-17 13:40:42 +0300

    Alıştırmalar düzeltmeler

--- Alıştırmalar/alıştırma_1.4.py
+++ Alıştırmalar/alıştırma_1.4.py
@@ -2,10 +2,11 @@ try:
     hrs = float(input("Enter hours: "))
     if hrs <= 40:
         rate = 10
+        pay = hrs * rate
     else:
         rate = 15
 
-    pay = hrs * rate
+    pay = ((hrs - 40) * rate) + 400
     print("Your pay:", pay)
 except:
     print('Please enter a number')
